55a9e8f7ab42f8f6f1b8276fdb70d41307130f67
lcy0x1 <chengyilin@umail.ucsb.edu>
2024-04-01 20:31:15 +0100

    conditional fix

--- src/main/java/dev/xkmc/l2library/capability/conditionals/ConditionalData.java
+++ src/main/java/dev/xkmc/l2library/capability/conditionals/ConditionalData.java
@@ -14,6 +14,7 @@ import net.minecraftforge.common.capabilities.CapabilityToken;
 
 import javax.annotation.Nullable;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 @SerialClass
 public class ConditionalData extends PlayerCapabilityTemplate<ConditionalData> {
@@ -54,7 +55,11 @@ public class ConditionalData extends PlayerCapabilityTemplate<ConditionalData> {
 				tickSinceDeath < 60 && player.getHealth() < player.getMaxHealth()) {
 			player.setHealth(player.getMaxHealth());
 		}
-		data.entrySet().removeIf(e -> e.getValue().tick(player));
+		var copy = data;
+		data = new HashMap<>();
+		copy.entrySet().removeIf(e -> e.getValue().tick(player));
+		copy.putAll(data);
+		data = copy;
 	}
 
 	public boolean hasData(TokenKey<?> eff) {
