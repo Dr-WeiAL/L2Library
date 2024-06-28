c8d26657c6b9efaf8f4cee14d2a5d9ba3b80f05a
lcy0x1 <chengyilin@umail.ucsb.edu>
2024-06-28 09:39:47 +0800

    remove overlay

--- src/main/java/dev/xkmc/l2library/base/overlay/package-info.java
+++
@@ -1,8 +0,0 @@
-@MethodsReturnNonnullByDefault
-@ParametersAreNonnullByDefault
-
-package dev.xkmc.l2library.base.overlay;
-
-import net.minecraft.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
